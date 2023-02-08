951e4e6aaf5f6e53948f372ffc8c220893fd0dfb
LuongTanDai <dai.luong@sai-digital.com>
2023-02-08 13:21:13 +0700

    fixing some issue with ui 2

--- src/components/Layout/Popper/Menu/index.js
+++ src/components/Layout/Popper/Menu/index.js
@@ -1,57 +1,74 @@
 import Tippy from "@tippyjs/react/headless";
 import classNames from "classnames/bind";
-import styles from './Menu.module.scss';
+import styles from "./Menu.module.scss";
 import { Wrapper as PopperWrapper } from "~/components/Layout/Popper";
-import MenuItem from './MenuItem';
+import MenuItem from "./MenuItem";
 import Header from "./Header";
-import { useState } from 'react';
+import { useState } from "react";
 
 const cx = classNames.bind(styles);
 
-const defaultFn = () =>{}
+const defaultFn = () => {};
 
-function Menu({ children, items = [], onChange = defaultFn, hideOnClick = false }) {
+function Menu({
+  children,
+  items = [],
+  onChange = defaultFn,
+  hideOnClick = false,
+}) {
+  const [history, setHistory] = useState([{ data: items }]);
 
-    const [history, setHistory] = useState([{ data: items }]);
+  const current = history[history.length - 1];
 
-    const current = history[history.length - 1];
+  const renderItems = () => {
+    return current.data.map((item, i) => {
+      const isParent = !!item.children;
 
-    const renderItems = () => {
-        return current.data.map((item, i) => {
-            const isParent = !!item.children;
+      return (
+        <MenuItem
+          key={i}
+          data={item}
+          onClick={() => {
+            if (isParent) {
+              setHistory((prev) => [...prev, item.children]);
+            } else {
+              onChange(item);
+            }
+          }}
+        ></MenuItem>
+      );
+    });
+  };
 
-            return (<MenuItem key={i} data={item} onClick={() => {
-                if (isParent) {
-                    setHistory(prev => [...prev, item.children])
-                } else {
-                    onChange(item)
-                }
-            }}></MenuItem>)
-        })
-    }
-
-    return (
-        <Tippy
-            interactive
-            delay={[0, 700]}
-            offset={[12, 8]}
-            hideOnClick={hideOnClick}
-            placement="bottom-end"
-            render={attrs => (
-                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx('menu-popper')}>
-                        {history.length > 1 && <Header title="Language" onBack={()=>{
-                            setHistory(prev => prev.slice(0, prev.length - 1))
-                        }}></Header>}
-                        {renderItems()}
-                    </PopperWrapper>
-                </div>
+  return (
+    <Tippy
+      interactive
+      delay={[0, 700]}
+      offset={[12, 8]}
+      hideOnClick={hideOnClick}
+      placement="bottom-end"
+      render={(attrs) => (
+        <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
+          <PopperWrapper className={cx("menu-popper")}>
+            {history.length > 1 && (
+              <Header
+                title="Language"
+                onBack={() => {
+                  setHistory((prev) => prev.slice(0, prev.length - 1));
+                }}
+              ></Header>
             )}
-            onHide={()=>{setHistory(prev => prev.slice(0, 1))}}
-        >
-            {children}
-        </Tippy>
-    );
+            <div className={cx("menu-body")}>{renderItems()}</div>
+          </PopperWrapper>
+        </div>
+      )}
+      onHide={() => {
+        setHistory((prev) => prev.slice(0, 1));
+      }}
+    >
+      {children}
+    </Tippy>
+  );
 }
 
 export default Menu;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,9 @@
 function Home() {
-    return ( <>
-    <h2>Home Page</h2>
-    </> );
+  return (
+    <>
+      <h2>Home Page</h2>
+    </>
+  );
 }
 
 export default Home;
